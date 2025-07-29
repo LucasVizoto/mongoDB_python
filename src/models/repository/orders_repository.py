25cf7d2632303321b9869aef622da4db39d6f0bc
Lucas Vizoto <lucasvizoto364@gmail.com>
2025-07-29 06:30:49 -0300

    feat: implementing select action wiyh mongo

--- src/models/repository/orders_repository.py
+++ src/models/repository/orders_repository.py
@@ -1,3 +1,5 @@
+from bson.objectid import ObjectId
+
 class OrdersRepository:
     def __init__(self, db_connection) -> None:
         self.__collection_name = 'orders'
@@ -11,3 +13,40 @@ class OrdersRepository:
     def insert_list_of_documents(self, list_of_documents: list) -> None:
         collection = self.__db_connection.get_collection(self.__collection_name)
         collection.insert_many(list_of_documents)
+
+    def select_many(self, doc_filter: dict) -> list[dict]:
+        collection = self.__db_connection.get_collection(self.__collection_name)
+        data = collection.find(doc_filter)
+        return data
+    
+    def select_one(self, doc_filter: dict)->dict:
+        collection = self.__db_connection.get_collection(self.__collection_name)
+        response = collection.find_one(doc_filter)
+        return response
+    
+    def select_many_with_properties(self, doc_filter: dict)->list:
+        collection = self.__db_connection.get_collection(self.__collection_name)
+        property_doc = {"_id": 0, "cupom": 0} #opções de retorno
+        data = collection.find(
+            doc_filter, #filtro de busca
+            property_doc #propriedades que serão removidas da response
+            )
+        return data
+
+    def select_if_property_exists(self)->dict:
+        collection = self.__db_connection.get_collection(self.__collection_name)
+        
+        exist_doc = {
+            "address": {"$exists": True}
+        }
+
+        property_doc = {"_id": 0, "itens": 0}
+
+        data = collection.find(exist_doc, property_doc)
+        return data
+    
+    def select_by_object_id(self, object_id: str) -> dict:
+        collection = self.__db_connection.get_collection(self.__collection_name)
+        data = collection.find_one({"_id": ObjectId(object_id)})
+        return data
+    
