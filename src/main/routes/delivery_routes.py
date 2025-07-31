0642bdcba2bab9ccdf3b1744472f42c32e8647c9
Lucas Vizoto <lucasvizoto364@gmail.com>
2025-07-31 06:11:07 -0300

    feat: implementing http_types

--- src/main/routes/delivery_routes.py
+++ src/main/routes/delivery_routes.py
@@ -1,8 +1,15 @@
 from flask import Blueprint, jsonify, request
 
+from src.main.http_types.http_request import HttpRequest
+
 delivery_routes_bp = Blueprint('delivery_routes', __name__)
 
 @delivery_routes_bp.route('/delivery/order', methods=['POST'])
 def registry_order():
-    print(request.json)
+    http_request = request.json
+
+    #levar o http_request para a lógica
+    #lógica retorna a response
+    
+
     return jsonify({"hello": "world"}), 200
