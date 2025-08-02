1784da87cb79548d92bbad8a24b7fb2f690c211c
Lucas Vizoto <lucasvizoto364@gmail.com>
2025-08-02 17:20:17 -0300

    feat: Adding use caso in registry order route

--- src/main/routes/delivery_routes.py
+++ src/main/routes/delivery_routes.py
@@ -2,14 +2,16 @@ from flask import Blueprint, jsonify, request
 
 from src.main.http_types.http_request import HttpRequest
 
+from src.main.composer.registry_order_composer import registry_order_composer
+
 delivery_routes_bp = Blueprint('delivery_routes', __name__)
 
 @delivery_routes_bp.route('/delivery/order', methods=['POST'])
 def registry_order():
-    http_request = request.json
+    use_case = registry_order_composer()
+    http_request = HttpRequest(body=request.json)
 
-    #levar o http_request para a lógica
-    #lógica retorna a response
+    response = use_case.registry(http_request)
     
 
-    return jsonify({"hello": "world"}), 200
+    return jsonify(response.body), response.status_code
